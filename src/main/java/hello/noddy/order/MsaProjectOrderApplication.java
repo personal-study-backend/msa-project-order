14c3e4c005141196889eb02b94c8522b3aaeaba0
sangwoo0727 <kangsw9395@gmail.com>
2021-09-11 21:14:19 +0900

    first commit

---
+++ src/main/java/hello/noddy/order/MsaProjectOrderApplication.java
@@ -0,0 +1,13 @@
+package hello.noddy.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaProjectOrderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsaProjectOrderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hello/noddy/order/MsaProjectOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.noddy.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaProjectOrderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
